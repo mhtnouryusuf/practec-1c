ddca04ce06e9aa3e1e569031b776666e3de68286
mhtnouryusuf <mahamatnour4774@gmail.com>
2026-03-06 15:16:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from main import greet
 
 def test_greet():
-    assert greet("world") == ("Hello, world!")
-   
+    assert greet("world") == "Hello, world!"
+
+if __name__ == "__main__":
+    test_greet()
+    print("Test passed successfully!")
